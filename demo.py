492423d197a5e0e574a09bd9ad9062424c845e75
chiranjeevi <chiranjeevi@klh.edu.in>
2022-08-10 08:51:47 +0530

    10th August 2022

--- demo.py
+++ demo.py
@@ -1,3 +1,14 @@
-print("hi")
-print("Hello")
-print("PFSD")
+class Employee:
+    def __init__(self,first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    company = 'Youtube'
+
+    def get_email(self):
+        return f'{self.first_name}.{self.last_name}@{Employee.company}com'
+
+
+emp1 = Employee('John', 'Smith')
+print(emp1.get_email())
+print(emp1.last_name)
